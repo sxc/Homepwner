29426a6df1d86c15716b9b53f821e57f32e6dfb5
Shen Xiaochun <shendeveloper@gmail.com>
2017-04-06 09:43:36 +0800

    Content Insets

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -36,5 +36,14 @@ class ItemsViewController: UITableViewController {
     }
     
     
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get the height of the staus bar 
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
 }
