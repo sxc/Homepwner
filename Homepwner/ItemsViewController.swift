99f6f178e1e5cbc57ec1671f45e8e95379657471
Shen Xiaochun <shendeveloper@gmail.com>
2017-04-10 11:38:14 +0800

    add detail view controller and segue

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -140,4 +140,23 @@ class ItemsViewController: UITableViewController {
         return "Remove"
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // If the triggered segue is the "showItem" segue
+        switch segue.identifier {
+        case "showItem"?:
+            // Figure out which row was just tapped
+            if let row = tableView.indexPathForSelectedRow?.row {
+                
+                // Get the item associated with this row and pass it along
+                let item = itemStore.allItems[row]
+                let detailViewController = segue.destination as! DetailViewController
+                detailViewController.item = item
+                
+            }
+        default:
+            preconditionFailure("Unexpected segue identifier.")
+        }
+        
+    }
+    
 }
