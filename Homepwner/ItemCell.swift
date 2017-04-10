bde7d632ae515a2431616acd92db19182a2f37f8
Shen Xiaochun <shendeveloper@gmail.com>
2017-04-10 09:53:29 +0800

    add Dynamic Type

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,26 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Shen Xiaochun on 2017/4/7.
+//  Copyright © 2017年 Sigutian. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+        
+    }
+}
